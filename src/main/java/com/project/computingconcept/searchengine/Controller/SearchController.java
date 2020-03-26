faf20f2653cce000fae50b5810f05abc37615883
Aayushee Dave <aayusheedave@gmail.com>
2020-03-26 18:36:25 -0400

    Adding methods

--- src/main/java/com/project/computingconcept/searchengine/Controller/SearchController.java
+++ src/main/java/com/project/computingconcept/searchengine/Controller/SearchController.java
@@ -14,7 +14,7 @@ import java.io.IOException;
 public class SearchController {
 
     @Autowired
-    WebCrawlerService webCrawlerService;
+    private WebCrawlerService webCrawlerService;
 
     @GetMapping("/search/{key}")
     private void searchHTMLFiles(@PathVariable String key) throws IOException {
