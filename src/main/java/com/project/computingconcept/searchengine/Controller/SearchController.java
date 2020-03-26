bb4e5204c34d70025b5e0c400b4e9286cbfb772d
Rahul Pandya <rahul25796@gmail.com>
2020-03-26 17:44:53 -0400

    added code to predict words

--- src/main/java/com/project/computingconcept/searchengine/Controller/SearchController.java
+++ src/main/java/com/project/computingconcept/searchengine/Controller/SearchController.java
@@ -1,16 +1,30 @@
 package com.project.computingconcept.searchengine.Controller;
 
+import com.project.computingconcept.searchengine.Service.WordPredictService;
+import com.project.computingconcept.searchengine.ServiceImpl.WordPredictServiceImpl;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class SearchController {
+
+    private WordPredictService predictService = new WordPredictServiceImpl();
 
     @GetMapping("/search/{key}")
     private String searchHTMLFiles(@PathVariable String key) {
         return "This Works!!";
     }
+
+    @GetMapping("/autocomplete/{key}")
+    private List<String> autocomplete(@PathVariable String key) {
+        List<String> predictedWords;
+        predictedWords = predictService.autoCompleteWord(key);
+        return predictedWords;
+    }
+
 }
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -3,12 +3,12 @@ const showErrorMsg = false;
 const errorMsg = "Sorry we coundn't connect to server";
 
 function search(event) {
-  let value = $("#searchBox").value;
+  let value = $("#searchBox")[0].value;
 
   // if pressed enter
   if (event.code === "Enter") {
     // call our API
-    axios.get(URL + "/search/+" + value).then(response => {
+    axios.get(URL + "/autocomplete/" + value).then(response => {
       if (response && response.status === 200) {
         console.log("data: " + response.data);
         let tag = "<h4>" + response.data + "</h4>";
